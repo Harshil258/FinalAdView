df0804c2422ca4b27c1db2f6108ad056b746d77a
admin <harshilvekariya12345@gmail.com>
2022-04-14 11:12:55 +0530

    with native and interstitial

--- CustomAdView/src/main/java/com/harshil/customadview/CustomInterstitial.kt
+++ CustomAdView/src/main/java/com/harshil/customadview/CustomInterstitial.kt
@@ -8,30 +8,43 @@ import com.applovin.mediation.MaxError
 import com.applovin.mediation.ads.MaxInterstitialAd
 
 class CustomInterstitial {
+
+    var interstitialfrequancy: Int = 1
+    var interstitialcount: Int = 1
+
+    constructor(interstitialfrequancy: Int) {
+        this.interstitialfrequancy = interstitialfrequancy
+    }
+
+
     fun showInterstitial(activity: Activity?, maxinterstitial: String) {
-        lateinit var interstitialAd: MaxInterstitialAd
-        interstitialAd = MaxInterstitialAd(maxinterstitial, activity)
-        interstitialAd.setListener(object : MaxAdListener {
-            override fun onAdLoaded(ad: MaxAd?) {
-                interstitialAd.showAd()
-                Log.d("showInterstitial", "interstitial loded")
-            }
 
-            override fun onAdDisplayed(ad: MaxAd?) {
-            }
+        if (interstitialcount == interstitialfrequancy.toInt()) {
+            lateinit var interstitialAd: MaxInterstitialAd
+            interstitialAd = MaxInterstitialAd(maxinterstitial, activity)
+            interstitialAd.setListener(object : MaxAdListener {
+                override fun onAdLoaded(ad: MaxAd?) {
+                    interstitialAd.showAd()
+                    Log.d("showInterstitial", "interstitial loded")
+                }
 
-            override fun onAdHidden(ad: MaxAd?) {
-            }
+                override fun onAdDisplayed(ad: MaxAd?) {
+                }
 
-            override fun onAdClicked(maxAd: MaxAd) {}
-            override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
-                Log.d("showInterstitial", "ad load failed interstitial ${error.toString()}")
-            }
+                override fun onAdHidden(ad: MaxAd?) {
+                }
 
-            override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
-                interstitialAd.loadAd()
-            }
-        })
-        interstitialAd.loadAd()
+                override fun onAdClicked(maxAd: MaxAd) {}
+                override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
+                    Log.d("showInterstitial", "ad load failed interstitial ${error.toString()}")
+                }
+
+                override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
+                    interstitialAd.loadAd()
+                }
+            })
+            interstitialAd.loadAd()
+            interstitialcount++
+        }
     }
 }
