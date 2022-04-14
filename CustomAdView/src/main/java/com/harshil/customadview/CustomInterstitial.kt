41d26b0fa8aff7f401a74ac6f8625809ff1a324e
admin <harshilvekariya12345@gmail.com>
2022-04-14 11:55:34 +0530

    with rewarded ads

--- CustomAdView/src/main/java/com/harshil/customadview/CustomInterstitial.kt
+++ CustomAdView/src/main/java/com/harshil/customadview/CustomInterstitial.kt
@@ -2,10 +2,9 @@ package com.harshil.customadview
 
 import android.app.Activity
 import android.util.Log
-import com.applovin.mediation.MaxAd
-import com.applovin.mediation.MaxAdListener
-import com.applovin.mediation.MaxError
+import com.applovin.mediation.*
 import com.applovin.mediation.ads.MaxInterstitialAd
+import com.applovin.mediation.ads.MaxRewardedAd
 
 class CustomInterstitial {
 
@@ -16,6 +15,45 @@ class CustomInterstitial {
         this.interstitialfrequancy = interstitialfrequancy
     }
 
+    fun showRewardedads(activity: Activity?, maxrewarded: String, maxinterstitial: String) {
+
+        lateinit var rewardedAd: MaxRewardedAd
+        rewardedAd = MaxRewardedAd.getInstance(maxrewarded, activity)
+        var maxRewardedAdListener = object : MaxRewardedAdListener {
+            override fun onAdLoaded(ad: MaxAd?) {
+                Log.d("showRewardedads", "Rewardedads loded")
+                rewardedAd.showAd()
+            }
+
+            override fun onAdDisplayed(ad: MaxAd?) {
+            }
+
+            override fun onAdHidden(ad: MaxAd?) {
+            }
+
+            override fun onAdClicked(ad: MaxAd?) {
+            }
+
+            override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
+                Log.d("showRewardedads", "Rewardedads Failed")
+                showInterstitial(activity, maxinterstitial)
+            }
+
+            override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
+            }
+
+            override fun onRewardedVideoStarted(ad: MaxAd?) {
+            }
+
+            override fun onRewardedVideoCompleted(ad: MaxAd?) {
+            }
+
+            override fun onUserRewarded(ad: MaxAd?, reward: MaxReward?) {
+            }
+        }
+        rewardedAd.setListener(maxRewardedAdListener)
+        rewardedAd.loadAd()
+    }
 
     fun showInterstitial(activity: Activity?, maxinterstitial: String) {
 
