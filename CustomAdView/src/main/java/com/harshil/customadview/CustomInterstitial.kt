182b409abe19dcaa91dd072e2fad10515daaa021
admin <harshilvekariya12345@gmail.com>
2022-05-20 11:14:38 +0530

    interstitial fix

--- CustomAdView/src/main/java/com/harshil/customadview/CustomInterstitial.kt
+++ CustomAdView/src/main/java/com/harshil/customadview/CustomInterstitial.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import com.applovin.mediation.*
 import com.applovin.mediation.ads.MaxInterstitialAd
 import com.applovin.mediation.ads.MaxRewardedAd
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class CustomInterstitial {
 
@@ -15,7 +18,13 @@ class CustomInterstitial {
         this.interstitialfrequancy = interstitialfrequancy
     }
 
-    fun showRewardedads(activity: Activity?, maxrewarded: String, maxinterstitial: String) {
+    fun showRewardedads(
+        isgoogle: Boolean,
+        activity: Activity?,
+        maxrewarded: String,
+        maxinterstitial: String,
+        googleinterstitial: String
+    ) {
 
         lateinit var rewardedAd: MaxRewardedAd
         rewardedAd = MaxRewardedAd.getInstance(maxrewarded, activity)
@@ -36,7 +45,7 @@ class CustomInterstitial {
 
             override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
                 Log.d("showRewardedads", "Rewardedads Failed")
-                showInterstitial(activity, maxinterstitial)
+                showInterstitial(isgoogle, activity, maxinterstitial, googleinterstitial)
             }
 
             override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
@@ -55,34 +64,71 @@ class CustomInterstitial {
         rewardedAd.loadAd()
     }
 
-    fun showInterstitial(activity: Activity?, maxinterstitial: String) {
+    fun showInterstitial(
+        isgoogle: Boolean,
+        activity: Activity?,
+        maxinterstitial: String,
+        googleinterstitial: String
+    ) {
 
-        if (interstitialcount % interstitialfrequancy.toInt() == 0) {
-            lateinit var interstitialAd: MaxInterstitialAd
-            interstitialAd = MaxInterstitialAd(maxinterstitial, activity)
-            interstitialAd.setListener(object : MaxAdListener {
-                override fun onAdLoaded(ad: MaxAd?) {
-                    interstitialAd.showAd()
-                    Log.d("showInterstitial", "interstitial loded")
-                }
 
-                override fun onAdDisplayed(ad: MaxAd?) {
-                }
+        if (isgoogle == true) {
+            val mInterstitialAd =
+                arrayOfNulls<com.google.android.gms.ads.interstitial.InterstitialAd>(1)
+            val adRequest = AdRequest.Builder().build()
+            com.google.android.gms.ads.interstitial.InterstitialAd.load(
+                activity,
+                googleinterstitial,
+                adRequest,
+                object : InterstitialAdLoadCallback() {
+                    override fun onAdLoaded(googleinterstitialAd: com.google.android.gms.ads.interstitial.InterstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd[0] = googleinterstitialAd
+                        Log.i("showInterstitial", "google interstitial onAdLoaded")
+                        if (mInterstitialAd[0] != null) {
+                            mInterstitialAd[0]!!.show((activity)!!)
+                        } else {
+                            Log.d("showInterstitial", "The interstitial ad wasn't ready yet.")
+                        }
+                    }
 
-                override fun onAdHidden(ad: MaxAd?) {
-                }
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        // Handle the error
+                        Log.i("showInterstitial", loadAdError.message)
+                        mInterstitialAd[0] = null
+                    }
+                })
+        } else {
+            if (interstitialcount % interstitialfrequancy.toInt() == 0) {
+                lateinit var interstitialAd: MaxInterstitialAd
+                interstitialAd = MaxInterstitialAd(maxinterstitial, activity)
+                interstitialAd.setListener(object : MaxAdListener {
+                    override fun onAdLoaded(ad: MaxAd?) {
+                        interstitialAd.showAd()
+                        Log.d("showInterstitial", "interstitial loded")
+                    }
 
-                override fun onAdClicked(maxAd: MaxAd) {}
-                override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
-                    Log.d("showInterstitial", "ad load failed interstitial ${error.toString()}")
-                }
+                    override fun onAdDisplayed(ad: MaxAd?) {
+                    }
 
-                override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
-                    interstitialAd.loadAd()
-                }
-            })
-            interstitialAd.loadAd()
-            interstitialcount++
+                    override fun onAdHidden(ad: MaxAd?) {
+                    }
+
+                    override fun onAdClicked(maxAd: MaxAd) {}
+                    override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
+                        Log.d("showInterstitial", "ad load failed interstitial ${error.toString()}")
+                    }
+
+                    override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
+                        interstitialAd.loadAd()
+                    }
+                })
+                interstitialAd.loadAd()
+                interstitialcount++
+            }
         }
+
+
     }
 }
